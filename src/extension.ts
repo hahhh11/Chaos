b667809041f292f8411520d89da12a3308d5396a
Joker Huang <hahhh11@live.com>
2020-12-06 21:55:24 +0800

    1

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,15 @@ export function activate(context: vscode.ExtensionContext) {
     // 实现树视图的初始化
 	// TreeViewProvider.initTreeViewItem();
 	addEvent('Chaos.todos.addItem',()=>{
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding', // Identifies the type of the webview. Used internally
+			'Cat Coding', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		  );
 
+		
+		  panel.webview.html = getWebviewContent();
 	});
 
 	addEvent('Chaos.openFile', () => {
@@ -41,5 +49,19 @@ export function activate(context: vscode.ExtensionContext) {
 	function addEvent(command:string,callback:(...args:any[])=>any,thisArg?:any){
 		context.subscriptions.push(vscode.commands.registerCommand(command, callback));
 	}
+
+	function getWebviewContent() {
+		return `<!DOCTYPE html>
+	  <html lang="en">
+	  <head>
+		  <meta charset="UTF-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		  <title>Cat Coding</title>
+	  </head>
+	  <body>
+		  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	  </body>
+	  </html>`;
+	  }
 }
 export function deactivate() {}
